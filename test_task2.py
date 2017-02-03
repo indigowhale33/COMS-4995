cbed959896f599f37f4845c7e80f2d61e79978d3
GwonJae Cho <gc2708@columbia.edu>
2017-02-03 07:35:49 -0500

    commit

---
+++ test_task2.py
@@ -0,0 +1,31 @@
+from __future__ import print_function, division
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+#from sklearn.cross_validation import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier as KNN
+
+#import pytest
+
+def division(x,y):
+	return x/y
+
+def test_div():
+	assert division(2,8) == 0.25
+
+def test_np_div():
+	two = np.array([2])
+	eight = np.array([8])
+	assert division(two,eight) == 0.25
+
+def test_input():
+	with open('input.txt', 'r') as f:
+		assert len(f.read()) == 7
+
+def test_KNN():
+	iris = load_iris()
+	X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.4, random_state=42)
+	knn = KNN(5)
+	scores = cross_val_score(knn, X_test, y_test, cv=5, scoring='accuracy')
+	assert scores.mean() > 0.7
